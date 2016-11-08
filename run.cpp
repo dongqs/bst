9758ee719de1c2483091b399b0f0392014d8ddd9
dongqs <dongqs@gmail.com>
2016-11-09 00:00:10 +0800

    some operations

--- run.cpp
+++ run.cpp
@@ -7,6 +7,15 @@ int main()
 {
     Node* node = new Node(5);
 
+    // Node* result = NULL;
+    // result = node->search(3);
+
+    // node->insert(3);
+    // node->insert(7);
+    // node->insert(2);
+
+    // delete node .....
+
     cout << node->val << endl;
     return 0;
 }
